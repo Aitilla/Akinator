39517925270736db9338db5b6a8d547c459685b2
Aitilla <jhertaas@gmail.com>
2023-09-14 14:28:59 +0200

    kode

--- akinator.py
+++ akinator.py
@@ -11,6 +11,7 @@ def getRandomType():
     return random.choice(typeList)
 
 def type2Guess():
+    pokemonType = getRandomType()
 
 def type1Guess():
     pokemonType = getRandomType()  
@@ -28,13 +29,16 @@ def typeGuess():
 
 def typeGuess():
     for i in range(allowedQuestions):  
-        multiType = input('Does your Pokemon have more than one type?')
+        multiType = input('Does your Pokemon have more than one type?\n [y] or [n]?')
         if multiType == 'y':
             multiType = True
-            type1Guess
-        else:
+            type1Guess()
+        elif multiType == 'n':
             print('\nYour Pokemon is only one type okay.')
             typeGuess()
+        elif multiType != 'y' or 'n':
+            print('Invalid input, please try again.')
+            return typeGuess()
 
 def intro():
     print('\nWelcome to "Akinator"')
